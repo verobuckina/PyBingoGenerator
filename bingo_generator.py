574eb232a1d9fc2b27eaa86ddbd7a6ef87829c15
Veronica Buckina <buckina@gmail.com>
2022-08-14 15:08:01 +0200

    Add basic exception handling.

--- bingo_generator.py
+++ bingo_generator.py
@@ -8,33 +8,42 @@ list = [] # list of all possible bingo field fillers
 path = './ESC-Bingo-Fields.txt'
 
 print('------ Bingo Generator ------')
-print('Enter number of players: ')
-count = int(input())
-print('Enter size of ?? (i.e. for 5x5 enter 5): ')
-size =int(input())
+try: 
+    print('Enter number of players: ')
+    count = int(input())
 
-print('Enter path to txt file with Bingo entrys:')
-path = input()
+    print('Enter size of bingo card (i.e. for 5x5 enter 5): ')
+    size =int(input())
 
-with open(path, encoding='utf8') as f:
-    list = f.readlines()
-    f.close()
+    print('Enter path to txt file with Bingo entrys:')
+    path = input()
 
-list = np.array(list)
+    with open(path, encoding='utf8') as f:
+        list = f.readlines()
+        f.close()
 
-for user in range(count):
-    rng = np.random.default_rng()
-    list_identifiers = rng.choice(a=list, size=(size**2), replace=False)
+    list = np.array(list)
 
-    # here: export the stuff to plt
-    plt.figure(figsize=(15,15), facecolor='xkcd:light blue grey')
-    
-    for i in range(1, size**2 + 1):
-        subplt = plt.subplot(size, size, i)
-        plt.xticks([])
-        plt.yticks([]) 
-        plt.text(0.5, 0.5, twp.fill(list_identifiers[i-1], 25), horizontalalignment='center', verticalalignment='center')
+    for user in range(count):
+        rng = np.random.default_rng()
+        list_identifiers = rng.choice(a=list, size=(size**2), replace=False)
 
-    file_name = 'Bingo_nr' + str(user) + '.jpg'
-    
-    plt.savefig(file_name)
+        # here: export the stuff to plt
+        plt.figure(figsize=(15,15), facecolor='xkcd:light blue grey')
+        
+        for i in range(1, size**2 + 1):
+            subplt = plt.subplot(size, size, i)
+            plt.xticks([])
+            plt.yticks([]) 
+            plt.text(0.5, 0.5, twp.fill(list_identifiers[i-1], 25), horizontalalignment='center', verticalalignment='center')
+
+        file_name = 'Bingo_nr' + str(user) + '.jpg'
+        
+        plt.savefig(file_name)
+        
+except KeyboardInterrupt:
+    print("\nExiting...")
+    exit()
+except ValueError:
+    print("Invalid input. Input has to be an integer.")
+    exit()
