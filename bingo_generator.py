0753a4c1e6d4267730b4d87d90e1d0172978823b
Veronica Buckina <buckina@gmail.com>
2023-04-23 11:19:21 +0200

    Change user input.

--- bingo_generator.py
+++ bingo_generator.py
@@ -1,60 +1,58 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import textwrap as twp
+import argparse
 
-count = 1 # input from user on number of unique bingo cards to generate
-size = 5 # Input from user on side length of square bingo matrix
-list = [] # list of all possible bingo field fillers
-path = './ESC-Bingo-Fields.txt'
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--num_players', type=int, default=1)
+parser.add_argument('-f', '--num_fileds', type=int, default=25)
+parser.add_argumnet('-p', '--fields_path', type=str)
+parser.add_argument('-o', '--out_path', type=str)
+args = parser.parse_args()
 
-print('------ Bingo Generator ------')
-try: 
-    print('Enter number of players: ')
-    try:
-        count = int(input())
-    except ValueError:
-        print("Invalid input. Input has to be an integer.")
+def main():
+    if not args.fields_path and not args.out_path:
+        return
+
+    count = args.num_palyers # input from user on number of unique bingo cards to generate
+    size = args.num_fields # Input from user on side length of square bingo matrix
+    list = [] # list of all possible bingo field fillers
+    path = args.fields_path
+
+    print('------ Bingo Generator ------')
+    try: 
+        with open(path, encoding='utf8') as f:
+            list = f.readlines()
+            f.close()
+
+        list = np.array(list)
+        try:
+            if(list.size < (size)):
+                raise ValueError
+        except ValueError:
+            print("To few entrys in file.")
+            exit()
+
+        for user in range(count):
+            rng = np.random.default_rng()
+            list_identifiers = rng.choice(a=list, size=(size**2), replace=False)
+
+            # here: export the stuff to plt
+            plt.figure(figsize=(15,15), facecolor='xkcd:light blue grey')
+            
+            for i in range(1, size + 1):
+                subplt = plt.subplot(size, size, i)
+                plt.xticks([])
+                plt.yticks([]) 
+                plt.text(0.5, 0.5, twp.fill(list_identifiers[i-1], 25), horizontalalignment='center', verticalalignment='center')
+
+            file_name = 'Bingo_nr' + str(user) + '.jpg'
+            
+            plt.savefig(file_name)
+
+    except KeyboardInterrupt:
+        print("\nExiting...")
         exit()
 
-    print('Enter size of bingo card (i.e. for 5x5 enter 5): ')
-    try:
-        size =int(input())
-    except ValueError:
-        print("Invalid input. Input has to be an integer.")
-        exit()
-
-    print('Enter path to txt file with Bingo entrys:')
-    path = input()
-
-    with open(path, encoding='utf8') as f:
-        list = f.readlines()
-        f.close()
-
-    list = np.array(list)
-    try:
-        if(list.size < (size*size)):
-            raise ValueError
-    except ValueError:
-        print("To few entrys in file.")
-        exit()
-
-    for user in range(count):
-        rng = np.random.default_rng()
-        list_identifiers = rng.choice(a=list, size=(size**2), replace=False)
-
-        # here: export the stuff to plt
-        plt.figure(figsize=(15,15), facecolor='xkcd:light blue grey')
-        
-        for i in range(1, size**2 + 1):
-            subplt = plt.subplot(size, size, i)
-            plt.xticks([])
-            plt.yticks([]) 
-            plt.text(0.5, 0.5, twp.fill(list_identifiers[i-1], 25), horizontalalignment='center', verticalalignment='center')
-
-        file_name = 'Bingo_nr' + str(user) + '.jpg'
-        
-        plt.savefig(file_name)
-
-except KeyboardInterrupt:
-    print("\nExiting...")
-    exit()
+if __name__ == '__main__':
+    main()
