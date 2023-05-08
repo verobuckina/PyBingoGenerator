aacbac3f7b2f7725606d00e543302e361efe855a
Veronica Buckina <buckina@gmail.com>
2023-05-08 20:02:58 +0200

    Small changes.

--- bingo_generator.py
+++ bingo_generator.py
@@ -1,23 +1,25 @@
 import textwrap as twp
 import argparse
 import random
-from PIL import Image, ImageDraw, ImageFont
+import math
+from PIL import Image, ImageDraw
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--num_players', type=int, default=1)
-parser.add_argument('-f', '--num_fields', type=int, default=5)
-parser.add_argument('-fp', '--fields_path', type=str)
+parser.add_argument('-f', '--num_fields', type=int, default=25)
+parser.add_argument('-fp', '--prompts_path', type=str)
 parser.add_argument('-o', '--out_path', type=str, default='.')
 args = parser.parse_args()
 
 def main():
-    if not args.fields_path and not args.out_path:
+    if not args.prompts_path:
+        print('You have to give the path for the .txt-file for the prompts.')
         return
 
-    count = args.num_players # input from user on number of unique bingo cards to generate
-    size = args.num_fields # Input from user on side length of square bingo matrix
+    num_players = args.num_players # input from user on number of unique bingo cards to generate
+    num_fields = args.num_fields # Input from user on side length of square bingo matrix
     list = [] # list of all possible bingo field fillers
-    path = args.fields_path
+    path = args.prompts_path
 
     print('------ Bingo Generator ------')
     try: 
@@ -25,31 +27,33 @@ def main():
             list = f.readlines()
             f.close()
 
+        size = int(math.sqrt(num_fields))
+        if int(size + 0.5) ** 2 != num_fields:
+            print('Number of fields has to be square.')
+            return
+ 
         # check if list has enough entrys
         len_list = len(list)
-        if len_list < (size * size):
+        if len_list < num_fields:
             print('Prompt list to short.')
             return
 
-        for user in range(count):
+        for user in range(num_players):
             img_size = (512, 512)
             image = Image.new(mode='L', size=img_size, color=255)
 
             cell_size = int(512 / size)
+            fontsize = 10
 
             draw = ImageDraw.Draw(image)
-
             for i in range(0, 512, cell_size):
                 line = ((i, 0), (i, 512))
                 draw.line(line, fill=128)
                 line = ((0, i), (512, i))
                 draw.line(line, fill=128)
             
-            rand_prompts = random.sample(list, size * size)
+            rand_prompts = random.sample(list, num_fields)
             i = 0
-
-            fontsize = 10
-
             for x in range(size):
                 for y in range(size):
                     prompt = rand_prompts[i]
@@ -60,7 +64,9 @@ def main():
                                 fill=0)
                     i += 1
 
-            image.save(f'{args.out_path}/Bingo_nr{user}.jpg')
+            card_path = f'{args.out_path}/Bingo_nr{user}.jpg'
+            image.save(card_path)
+            print(f'Bingo card saved as: {card_path}')
 
     except KeyboardInterrupt:
         print("\nExiting...")
